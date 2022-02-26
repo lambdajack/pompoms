11059a01f74e977467e89c95ba17e45aabfa9697
Jack Bizzell <bizzell.jw@gmail.com>
2022-02-26 19:44:48 +0000

    build

--- build/utils/sleep.js
+++ build/utils/sleep.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.waitOneMin = exports.sleep = void 0;
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 exports.sleep = sleep;
-const waitOneMin = async () => await (0, exports.sleep)(10);
+const waitOneMin = async () => await (0, exports.sleep)(60000);
 exports.waitOneMin = waitOneMin;
